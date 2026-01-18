6bf767a53a3e58f6acfbda05d80cfea82599ae24
Urs Hofmann <mail@hofmannu.org>
2026-01-18 07:29:24 +0100

    Cuda: removing deprecated properties from info window

--- src/cudaTools.cu
+++ src/cudaTools.cu
@@ -1,102 +1,77 @@
 #include "cudaTools.cuh"
 
-cudaTools::cudaTools()
-{
-	// get number of connected devices
-	cudaError_t err = cudaGetDeviceCount(&nDevices);
-	if (err != cudaSuccess)
-	{
-		printf("Could not receive a device count\n");
-		throw "CudaError";
-	}
+cudaTools::cudaTools() {
+  // get number of connected devices
+  cudaError_t err = cudaGetDeviceCount(&nDevices);
+  if (err != cudaSuccess) {
+    printf("Could not receive a device count\n");
+    throw "CudaError";
+  }
 
-	// query device properties for each attached GPU
-	cudaDeviceProp currProps;
-	for (int iDevice = 0; iDevice < nDevices; iDevice++)
-	{
-		err = cudaGetDeviceProperties(&currProps, iDevice);
-		if (err != cudaSuccess)
-		{
-			printf("Something went wrong while returning properties of device %d\n", iDevice);
-			throw "CudaError";
-		}
+  // query device properties for each attached GPU
+  cudaDeviceProp currProps;
+  for (int iDevice = 0; iDevice < nDevices; iDevice++) {
+    err = cudaGetDeviceProperties(&currProps, iDevice);
+    if (err != cudaSuccess) {
+      printf("Something went wrong while returning properties of device %d\n",
+             iDevice);
+      throw "CudaError";
+    }
 
-		props.push_back(std::move(currProps));
-	}
-	return;
+    props.push_back(std::move(currProps));
+  }
+  return;
 }
 
-void cudaTools::print_devProps()
-{
-	for (int iDevice = 0; iDevice < nDevices; iDevice++)
-		print_devProps(iDevice);
-	return;
+void cudaTools::print_devProps() {
+  for (int iDevice = 0; iDevice < nDevices; iDevice++)
+    print_devProps(iDevice);
+  return;
 }
 
 // prints the properties of a device to the terminal
-void cudaTools::print_devProps(const int iDevice)
-{
-	if (iDevice < 0)
-	{
-		printf("Device index must be larger or equal 0!\n");
-		throw "InvalidValue";
-	}
+void cudaTools::print_devProps(const int iDevice) {
+  if (iDevice < 0) {
+    printf("Device index must be larger or equal 0!\n");
+    throw "InvalidValue";
+  }
 
-	if (iDevice >= nDevices)
-	{
-		printf("Requested ID is exceedin available device number\n");
-		throw "InvalidValue";
-	}
+  if (iDevice >= nDevices) {
+    printf("Requested ID is exceedin available device number\n");
+    throw "InvalidValue";
+  }
 
-	printf("General information for device %d: \n", iDevice);
-	printf(" - Name: %s\n", props[iDevice].name);
-	printf(" - Compute capability: %d.%d\n", props[iDevice].major, props[iDevice].minor);
-	printf(" - Clock rate: %d Hz\n", props[iDevice].clockRate);
+  printf("General information for device %d: \n", iDevice);
+  printf(" - Name: %s\n", props[iDevice].name);
+  printf(" - Compute capability: %d.%d\n", props[iDevice].major,
+         props[iDevice].minor);
 
-	printf(" - Device copy overlap: ");
-	if (props[iDevice].deviceOverlap)
-		printf("Enabled!\n");
-	else
-		printf("Disabled!\n");
+  printf(" - Total global mem: %ld bytes\n", props[iDevice].totalGlobalMem);
+  printf(" - Total constant mem: %ld bytes\n", props[iDevice].totalConstMem);
+  printf(" - Max mem pitch: %ld\n", props[iDevice].memPitch);
+  printf(" - Texture alignment: %ld\n", props[iDevice].textureAlignment);
+  printf(" - Multiprocessor count: %d\n", props[iDevice].multiProcessorCount);
+  printf(" - Shared memory per MP: %d bytes\n",
+         props[iDevice].sharedMemPerBlock);
+  printf(" - Registers per MP: %d\n", props[iDevice].regsPerBlock);
+  printf(" - Threads in warp: %d\n", props[iDevice].warpSize);
+  printf(" - Max threads per block: %d\n", props[iDevice].maxThreadsPerBlock);
+  printf(" - Max thread dimensions: (%d, %d, %d)\n",
+         props[iDevice].maxThreadsDim[0], props[iDevice].maxThreadsDim[1],
+         props[iDevice].maxThreadsDim[2]);
+  printf(" - Max grid dimensions: (%d, %d, %d)\n",
+         props[iDevice].maxGridSize[0], props[iDevice].maxGridSize[1],
+         props[iDevice].maxGridSize[2]);
 
-	printf(" - Kernel execution timeout: ");
-	if (props[iDevice].kernelExecTimeoutEnabled)
-		printf("Enabled\n");
-	else
-		printf("Disabled!\n");
-
-	printf(" - Total global mem: %ld bytes\n", props[iDevice].totalGlobalMem);
-	printf(" - Total constant mem: %ld bytes\n", props[iDevice].totalConstMem);
-	printf(" - Max mem pitch: %ld\n", props[iDevice].memPitch);
-	printf(" - Texture alignment: %ld\n", props[iDevice].textureAlignment);
-	printf(" - Multiprocessor count: %d\n", props[iDevice].multiProcessorCount);
-	printf(" - Shared memory per MP: %d bytes\n", props[iDevice].sharedMemPerBlock);
-	printf(" - Registers per MP: %d\n", props[iDevice].regsPerBlock);
-	printf(" - Threads in warp: %d\n", props[iDevice].warpSize);
-	printf(" - Max threads per block: %d\n", props[iDevice].maxThreadsPerBlock);
-	printf(" - Max thread dimensions: (%d, %d, %d)\n",
-		props[iDevice].maxThreadsDim[0], 
-		props[iDevice].maxThreadsDim[1], 
-		props[iDevice].maxThreadsDim[2]
-		);
-	printf(" - Max grid dimensions: (%d, %d, %d)\n",
-		props[iDevice].maxGridSize[0], 
-		props[iDevice].maxGridSize[1], 
-		props[iDevice].maxGridSize[2]
-		);
-
-
-	printf("\n");
-	return;
+  printf("\n");
+  return;
 }
 
-void cudaTools::checkCudaErr(const cudaError_t& err, const char* msgErr)
-{
-	if (err != cudaSuccess)
-	{
-		printf("There was some CUDA error: %s, %s\n",
-			msgErr, cudaGetErrorString(err));
-		throw "CudaError";
-	}
-	return;
+void cudaTools::checkCudaErr(const cudaError_t &err, const char *msgErr) {
+  if (err != cudaSuccess) {
+    printf("There was some CUDA error: %s, %s\n", msgErr,
+           cudaGetErrorString(err));
+    throw "CudaError";
+  }
+  return;
 }
